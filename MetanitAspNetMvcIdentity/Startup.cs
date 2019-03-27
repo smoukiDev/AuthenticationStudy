20182e071320b703989548d64e0d2e7db3cf63b4
Serhii Maksymchuk <smakdealcase@gmail.com>
2019-03-27 18:26:05 +0200

    Added solution with Asp.Net MVC5 project with WebApi and Identity.

---
+++ MetanitAspNetMvcIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MetanitAspNetMvcIdentity.Startup))]
+namespace MetanitAspNetMvcIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
